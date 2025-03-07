3ac4ad1d197d265b3f81369da1c0366c527e90e9
Udai Singh Mehra <udai_mehra_2000@yahoo.com>
2025-03-07 11:01:52 +0530

    New commits

---
+++ src/trunc.go
@@ -0,0 +1,16 @@
+// Program to truncate a floating point number into an integer 
+// at the decimal point
+/* 
+Write a program which prompts the user to enter a floating point number and prints the integer which is a truncated version of the floating point number that was entered. Truncation is the process of removing the digits to the right of the decimal place.
+*/
+package main
+import (
+	"fmt"
+)
+var fnum int
+func main() {
+        fmt.Printf("Enter a float number:")
+        //num, err := 
+        fmt.Scan(&fnum)
+        fmt.Printf(fnum)
+}
